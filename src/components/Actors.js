32eb4e4d0523cf09185d8120094b25b33bf89563
Jose Luis Mirafuentes <joseluis0528@gmail.com>
2022-06-12 19:59:21 -0500

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,18 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorObject = actors.map(actor => {
+    return <div key={actor.name}>
+      <h2>{actor.name}</h2>
+      <ul>
+        {actor.movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+    </div>
+  })
+  return <div>
+    <h1>Actors Page</h1>
+    {actorObject}
+  </div>;
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieObject = movies.map(movie => {
+    const genres = movie.genres.map(genre => {
+      return <li key={genre}>{genre}</li>
+    })
+    return <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Runtime: {movie.time}</p>
+      <ul>
+        {genres}
+      </ul>
+    </div>
+  })
+
+  return <div>
+    <h1>Movies Page</h1>
+    {movieObject}
+  </div>;
 }
 
 export default Movies;
